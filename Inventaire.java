88d255014cf41688b86e30d0ab5e9ed49b867e57
kinbarii <99283591+kinbarii@users.noreply.github.com>
2023-10-28 19:30:41 +0000

    inventaire ajoute

---
+++ Inventaire.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventaire{
+
+private Map<Produit,Integer> inventaire;
+
+public Inventaire(){
+
+	this.inventaire = new HashMap<>();
+  }
+
+  public void AjouterProduit(Produit produit, int quantite){
+
+	inventaire.put(produit, quantite);
+	System.out.println("Produit ajoutee avec succes" + produit);
+
+
+  }
+
+  public void SupprimerProduit(){
+	System.out.println("Produit supprime avec succes" + produit);
+  }
+
+  public void AfficherInventaire(){
+	System.err.println("********************************");
+	System.out.println("Ceci est notre inventaire actuel");
+  }
+
+}
+
